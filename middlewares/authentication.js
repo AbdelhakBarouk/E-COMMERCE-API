7d28de161398dd1d36d9db6b02f4db3713317c1d
ABDELHAK BAROUK <abarouk69@gmail.com>
2023-12-30 13:37:01 +0100

    authentication middleware

---
+++ middlewares/authentication.js
@@ -0,0 +1,32 @@
+const CustomError = require("../errors");
+const { StatusCodes } = require("http-status-codes");
+const { isValideToken } = require("../utils");
+
+const authenticateUser = async (req, res, next) => {
+  const token = req.signedCookies.token;
+  if (!token) {
+    throw new CustomError.UnAuthenticatedError("Authentication Invalid");
+  }
+
+  //check if the token is valide
+  try {
+    const { name, role, userId } = isValideToken({ token });
+    req.user = { name, userId, role };
+    next();
+  } catch (error) {
+    throw new CustomError.UnAuthenticatedError("Authentication Invalid");
+  }
+};
+
+const authorizePermission = async (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnAuthorizedError(
+        "Unauthorized to access this route"
+      );
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizePermission };
